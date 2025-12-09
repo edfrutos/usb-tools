2a3457fa225ba6b164bfb8fe6656a1b867f9fb9a
edefrutos <16051333+edfrutos@users.noreply.github.com>
2025-12-09 19:01:09 +0100

    Initial Commit

---
+++ USBLabTests/USBLabTests.swift
@@ -0,0 +1,16 @@
+//
+//  USBLabTests.swift
+//  USBLabTests
+//
+//  Created by Eugenio de Frutos Sanchez on 9/12/25.
+//
+
+import Testing
+
+struct USBLabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ USBLabUITests/USBLabUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  USBLabUITestsLaunchTests.swift
+//  USBLabUITests
+//
+//  Created by Eugenio de Frutos Sanchez on 9/12/25.
+//
+
+import XCTest
+
+final class USBLabUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
